8dcab58001794ac1f252cd99eabacace61c5fbf8
devunderslash <devunderslash@hotmail.com>
2025-02-26 11:39:12 +0000

    PLATFORM - basic eks arch added

--- infrastructure/base-ec2-arch-refined/instances.tf
+++ infrastructure/base-ec2-arch-refined/instances.tf
@@ -18,8 +18,57 @@ resource "aws_instance" "private-web-instance" {
   key_name                    = aws_key_pair.basic-arch-generated_key.key_name
   associate_public_ip_address = false
   security_groups             = [aws_security_group.basic-arch-private-sg.id]
+  user_data = <<EOF
+    #!/bin/bash
+    cd /home/var
+    sudo apt-get update
+    sudo apt-get install python3-pip -y
+    sudo pip3 install ansible
+    sleep 30
+    cd /home/var/opt
+    git clone https://github.com/devunderslash/devops-sre-platform.git
+EOF
 
   tags = {
     Name = "basic-arch-private-web"
   }
 }
+
+# Install python onto the ec2 instance with remote provisioner
+# Tested, this works if needed.
+
+# resource "null_resource" "install_python" {
+#   provisioner "remote-exec" {
+#     inline = [
+#       "sudo yum install -y python3"
+#     ]
+#     connection {
+#       type        = "ssh"
+#       user        = "ec2-user"
+#       # private key is in the same directory as this file
+#       private_key = file("aws-basic-arch-keypair.pem")
+#       host        = aws_instance.public-web-instance.public_ip
+#     }
+#   }
+# }
+
+
+# *** Can't install python onto the private instance as it doesn't have ssh key on initial setup ***
+
+# Install python onto the ec2 instance with remote provisioner
+# resource "null_resource" "install_python_private" {
+#   provisioner "remote-exec" {
+#     inline = [
+#       "sudo yum install -y python3"
+#     ]
+
+#     connection {
+#       type        = "ssh"
+#       user        = "ec2-user"
+#       private_key = file("aws-basic-arch-keypair.pem")
+#       host        = aws_instance.private-web-instance.private_ip
+#     }
+#   }
+# }
+
+
---
+++ infrastructure/base-eks-arch/main.tf
@@ -0,0 +1,8 @@
+locals {
+  tags = {
+    created-by = "devunderslash"
+    managed-by = "terraform"
+    env        = "dev"
+    cluster-name = "dev-eks-cluster"
+  }
+}
